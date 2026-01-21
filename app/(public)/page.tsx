f8ce4f3c0570b8f0b22d9a6472c48e7b8bbd187b
Mijanur Rahman <mijan.connect@gmail.com>
2026-01-21 10:19:16 +0600

    Hero updated

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -3,7 +3,12 @@ import Hero from "../components/home/Hero";
 export default function Home() {
   return (
     <main>
-      <Hero />
+      <>
+        <Hero />
+        <div className="container mx-auto px-4">
+          <Hero />
+        </div>
+      </>
     </main>
   );
 }
--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -1,17 +1,10 @@
-import Image from "next/image";
-import React from "react";
-
 export default function Hero() {
   return (
     <>
-      <div>
-        <Image
-          src="/assets/hero-image.png"
-          alt="Hero Image"
-          width={1920}
-          height={600}
-          className="w-full h-auto"
-        />
+      <div className="w-full h-screen bg-[url('/assets/hero-image.png')] bg-cover bg-center bg-no-repeat flex items-center justify-center">
+        <div className="container mx-auto px-4">
+          <h1 className="text-[60px] font-bold text-primary">Connect with trusted tradespeople <br /> across the UK — fast</h1>
+        </div>
       </div>
     </>
   );
